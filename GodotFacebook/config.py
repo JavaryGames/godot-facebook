4f185e3759fd2b888002b99ac9180990b9b5dbbd
aldoram5 <master-sword5al@hotmail.com>
2018-01-20 16:41:04 -0600

    Updated config.py so it adds minSdkVersion without having us to mess with the template

--- GodotFacebook/config.py
+++ GodotFacebook/config.py
@@ -6,5 +6,8 @@ def configure(env):
 		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4.27.0'")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.android_add_java_dir("android/src/")
+		env.android_add_default_config("minSdkVersion 15")
+		env.android_add_default_config("targetSdkVersion 24")
+		env.android_add_default_config("compileSdkVersion 24")
 		env.disable_module()
 
